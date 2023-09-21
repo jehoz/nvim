8ba5181b29762b2509647d5696c1c0f7a87f2adf
holzmanj <jesseholzman96@gmail.com>
2023-09-21 08:47:25 -0400

    Add keybinds for lsp code formatting and buffer searching.  Purescript formatter.  Some normal vim settings.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,13 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({ buffer = bufnr })
+
+    -- set keybind to format code
+    local opts = { buffer = bufnr }
+    vim.keymap.set({ 'n', 'x' }, 'gq', function()
+        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+    end, opts)
 end)
 
 
@@ -16,6 +22,11 @@ require('mason-lspconfig').setup({
 
 
 require('lspconfig').lua_ls.setup({})
-require('lspconfig').purescriptls.setup({})
+require('lspconfig').purescriptls.setup({
+    settings = {
+        purescript = {
+            formatter = 'purs-tidy'
+        }
+    }
+})
 require('lspconfig').pylsp.setup({})
-
--- lua/jehoz/init.lua
+++ lua/jehoz/init.lua
@@ -8,6 +8,18 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- disable line wrap
+vim.opt.wrap = false
+
+-- highlight incremental search, turn off highlights after search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- scroll offset (8 line padding at top and bottom of view when scrolling)
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "80"
+
 -- Keybind remapping
 vim.g.mapleader = " "
 
