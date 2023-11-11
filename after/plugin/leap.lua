44d9aeb4f97d8c6be5a7794cf2e91cc8135d9644
holzmanj <jesseholzman96@gmail.com>
2023-11-11 11:29:26 -0500

    Forgot to commit reformat

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -2,7 +2,7 @@ local leap = require("leap")
 
 leap.add_default_mappings()
 
-vim.keymap.set({"n", "v"}, "<CR>", function()
+vim.keymap.set({ "n", "v" }, "<CR>", function()
     local current_window = vim.fn.win_getid()
-    leap.leap {target_windows = {current_window}}
+    leap.leap { target_windows = { current_window } }
 end)
