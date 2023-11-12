eb3176d45bb692c48e29083cbf718a6e706e544c
holzmanj <jesseholzman96@gmail.com>
2023-11-12 10:02:47 -0500

    Add telescope binding for finding files in cwd

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
-local builtin = require('telescope.builtin')
-local themes = require('telescope.themes')
+local builtin = require("telescope.builtin")
+local themes = require("telescope.themes")
+local utils = require("telescope.utils")
 
-require('telescope').setup {
+require("telescope").setup {
     extensions = {
         ["ui-select"] = {
             themes.get_dropdown {}
@@ -9,14 +10,18 @@ require('telescope').setup {
     }
 }
 
-require('telescope').load_extension('ui-select')
+require("telescope").load_extension("ui-select")
 
-vim.keymap.set('n', '<leader>ff', function()
-    builtin.find_files({ path_display = { "truncate" } })
+vim.keymap.set("n", "<leader>fd", builtin.find_files, {})
+
+vim.keymap.set("n", "<leader>ff", function()
+    builtin.find_files({ cwd = utils.buffer_dir() })
 end)
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 
-vim.keymap.set('n', '<leader>z', function()
+vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+
+vim.keymap.set("n", "<leader>z", function()
     builtin.spell_suggest(themes.get_cursor({}))
 end)
